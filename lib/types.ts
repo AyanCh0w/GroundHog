5e3b8cb9cf35ee10836f998d83c766db236b9e82
AyanCh0w <github@redbot.pro>
2025-07-29 14:08:09 -0400

    updated interface

--- lib/types.ts
+++ lib/types.ts
@@ -1,13 +1,10 @@
 export interface FarmLocation {
   id: number;
-  name: string;
+  farm_name: string;
+  farmer_name: string;
   created_at: string; // or `Date` if you'll parse it
   lat: number;
   long: number;
-  lat_bounds_one: number | null;
-  lat_bounds_two: number | null;
-  long_bounds_one: number | null;
-  long_bounds_two: number | null;
 }
 
 export interface ChemicalEstimate {
